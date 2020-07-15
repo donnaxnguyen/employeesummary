e0ac857b8b8981afce14d9ec01affcc261c20f16
donna <donnaxnguyen@yahoo.com>
2020-07-14 17:54:27 -0700

    started creating employee and engineer.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,29 @@
 // TODO: Write code to define and export the Employee class
+// this is the class -- employee
+class Employee {
+    // we want to create a name , id , and email object
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    // get name , and return the typed name
+    getname() {
+        return this.name;
+    }
+    // get id , and return the typed id
+    getId() {
+        return this.id;
+    }
+    // get email , and return the typed id 
+    getEmail() {
+        return this.email;
+    }
+    // this returns as an "employee"
+    getRole() {
+        return "Employee"
+    }
+}
+
+// exports the employee
+module.exports = Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+// require Employee class
+const employee = require("./Employee");
+
+// creating a class called engineer , which extends from Employee
+class Engineer extends Employee {
+    //  used super to call the parent constructor
+    constructor(name, id, email, github) {
+        super(name, id, email)
+        this.github = github;
+        }
+    
+    // get github info
+    getGithub(){
+        return this.github;
+    }
+
+    // outputs the "engineer" role
+    getRole(){
+        return "Engineer";
+    }
+}
+
+// exports the  employee
+module.exports = Engineer;
